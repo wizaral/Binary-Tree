b82f89fa694eea55fb5262ae480f7152d5bd8fb1
Alex Master <alex.macromaster@outlook.com>
2020-05-13 22:52:34 +0300

    rename vars, sugar enum & using, no move func, find, min, max, erase

--- tree.h
+++ tree.h
@@ -12,27 +12,22 @@ using T = int;
 class Tree final {
     struct Node {
         T data;
-        Node *parent = nullptr;
-        std::array<Node*, 2> nodes{nullptr, nullptr};    // [0] nodes[0], [1] nodes[1]
+        Node *prev = nullptr;
+        Node *next[2]{nullptr, nullptr};    // [0] next[L], [1] next[R]
     };
+    enum {L = 0, R = 1};
+
+    using Fcr = const std::function<void(const T &data)>;
+    using Fr = const std::function<void(T &data)>;
 
     Node *root_ = nullptr;
     std::size_t size_ = 0;
 public:
     Tree() = default;
 
-    // Tree(T &&...args) {
-    //     // std::forward<Args>(args)...
-    // }
-
     Tree(const Tree &tree) {
         merge(tree);
     }
-
-    // Tree(Tree &&tree) {
-    //     merge(std::move(tree));
-    //     tree.clear();
-    // }
 
     ~Tree() {
         clear();
@@ -45,16 +40,6 @@ public:
         size_ = temp.size_;
         return *this;
     }
-
-    // Tree &operator=(Tree &&tree) {
-    //     clear();
-    //     root_ = tree.root_;
-    //     size_ = tree.size_;
-
-    //     tree.root_ = nullptr;
-    //     tree.size_ = 0;
-    //     return *this;
-    // }
 
     bool empty() const {
         return size_ == 0;
@@ -80,19 +65,20 @@ public:
             for (Node *fast = root_, *slow = fast; true; slow = fast) {
                 if (data == fast->data) {
                     return false;
-                } else if (bool cmp = fast->data < data; !(fast = fast->nodes[cmp])) {
-                    slow->nodes[cmp] = new Node{data, slow};
+                } else if (bool cmp = fast->data < data; !(fast = fast->next[cmp])) {
+                    slow->next[cmp] = new Node{data, slow};
                     break;
                 }
             }
         }
-        ++size_;
-        return true;
+        return ++size_;
     }
-    // bool insert(T &&data) {
-    // }
 
     bool erase(const T &data) {
+        if (Node *node = find(root_, data); node) {
+            erase(node);
+            return true;
+        }
         return false;
     }
 
@@ -101,97 +87,117 @@ public:
             this->insert(data);
         });
     }
-    // void merge(Tree &&tree) {
-    //     tree.preorder([this](Node *node) {
-    //         this->insert(node->data);
-    //     });
-    // }
 
     bool find(const T &data) const {
         return find(root_, data) != nullptr;
     }
 
-    void preorder(const std::function<void(const T &data)> &f) const {
+    void preorder(Fcr &f) const {
         preorder(root_, f);
     }
 
-    void inorder(const std::function<void(const T &data)> &f) const {
+    void inorder(Fcr &f) const {
         inorder(root_, f);
     }
 
-    void postorder(const std::function<void(const T &data)> &f) const {
+    void postorder(Fcr &f) const {
         postorder(root_, f);
     }
 
 private:
     std::size_t height(const Node *node) const {
-        return node ? std::max(height(node->nodes[0]), height(node->nodes[1])) + 1 : 0;
+        return node ? std::max(height(node->next[L]), height(node->next[R])) + 1 : 0;
     }
 
     void clear(Node *node) {
         if (node) {
-            clear(node->nodes[0]);
-            clear(node->nodes[1]);
+            clear(node->next[L]);
+            clear(node->next[R]);
             delete node;
             --size_;
         }
     }
 
     Node *find(Node *node, const T &data) const {
-        if (node) {
-            if (data == node->data) {
-                return node;
-            } else {
-                return find(node->nodes[node->data < data], data);
-            }
-        }
-        return nullptr;
+        return !node || data == node->data ? node : find(node->next[node->data < data], data);
     }
 
-    void preorder(Node *node, const std::function<void(T &data)> &f) {
+    Node *min(Node *node) {
+        while (node->next[L])
+            node = node->next[L];
+        return node;
+    }
+
+    Node *max(Node *node) {
+        while (node->next[R])
+            node = node->next[R];
+        return node;
+    }
+
+    void erase(Node *node) {
+        if (node->next[L] && node->next[R]) {
+            Node *max_ = max(node->next[L]);
+            std::swap(node->data, max_->data);
+            return (erase(max_), void(0));
+        } else if (node->prev) {
+            if (node->next[L] || node->next[R])
+                node->prev->next[node == node->prev->next[R]] = node->next[node->next[R] != nullptr];
+            else
+                node->prev->next[node == node->prev->next[R]] = nullptr;
+        } else if (size_ > 1u) {
+            root_ = node->next[node->next[R] != nullptr];
+            root_->prev = nullptr;
+        } else {
+            root_ = nullptr;
+        }
+        delete node;
+        --size_;
+    }
+
+    void preorder(Node *node, Fr &f) {
         if (node) {
             f(node->data);
-            preorder(node->nodes[0], f);
-            preorder(node->nodes[1], f);
+            preorder(node->next[L], f);
+            preorder(node->next[R], f);
         }
     }
 
-    void inorder(Node *node, const std::function<void(T &data)> &f) {
+    void inorder(Node *node, Fr &f) {
         if (node) {
-            inorder(node->nodes[0], f);
+            inorder(node->next[L], f);
             f(node->data);
-            inorder(node->nodes[1], f);
+            inorder(node->next[R], f);
         }
     }
 
-    void postorder(Node *node, const std::function<void(T &data)> &f) {
+    void postorder(Node *node, Fr &f) {
         if (node) {
-            postorder(node->nodes[0], f);
-            postorder(node->nodes[1], f);
+            postorder(node->next[L], f);
+            postorder(node->next[R], f);
             f(node->data);
         }
     }
 
-    void preorder(const Node *node, const std::function<void(const T &data)> &f) const {
+    void preorder(const Node *node, Fcr &f) const {
         if (node) {
             f(node->data);
-            preorder(node->nodes[0], f);
-            preorder(node->nodes[1], f);
+            preorder(node->next[L], f);
+            preorder(node->next[R], f);
         }
     }
 
-    void inorder(const Node *node, const std::function<void(const T &data)> &f) const {
+    void inorder(const Node *node, Fcr &f) const {
         if (node) {
-            inorder(node->nodes[0], f);
+            inorder(node->next[L], f);
             f(node->data);
-            inorder(node->nodes[1], f);
+            inorder(node->next[R], f);
         }
     }
 
-    void postorder(const Node *node, const std::function<void(const T &data)> &f) const {
+    void postorder(const Node *node, Fcr &f) const {
         if (node) {
-            postorder(node->nodes[0], f);
-            postorder(node->nodes[1], f);
+            postorder(node->next[L], f);
+            postorder(node->next[R], f);
             f(node->data);
         }
     }
