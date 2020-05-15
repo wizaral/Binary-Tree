43e9d17907489eeefa97dd219fed7e1403acdfa3
Alex Master <alex.macromaster@outlook.com>
2020-05-16 00:01:03 +0300

    clean

--- main.cpp
+++ main.cpp
@@ -1,28 +1,7 @@
-#include <string>
-
 #include "tree.h"
-using std::cin, std::cout, std::endl;
+#include "menu.h"
 
 int main() {
-    Tree tree;
-
-    tree.insert(1);
-    tree.insert(11);
-    tree.insert(111);
-
-    tree.inorder([] (auto &&data) {
-        std::cout << data << std::endl;
-    });
-
-    Tree temp;
-    temp.insert(2);
-    temp.insert(22);
-    temp.insert(222);
-
-    tree.merge(temp);
-    tree.inorder([] (auto &&data) {
-        std::cout << data << std::endl;
-    });
-
-    
+    Tree<int> tree;
+    menu(tree);
 }
